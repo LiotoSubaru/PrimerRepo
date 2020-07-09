f20bbfc600d8c8c182bbbefd5cf33b1bf4ccd337
LiotoSubaru <johanballeza35@gmail.com>
2020-07-09 10:55:50 -0500

    Trabajo 3 terminado

---
+++ prog3.py
@@ -0,0 +1,40 @@
+menu = "0"
+
+while menu == "0":
+    menu = input("Elige una opción a continuación:")
+    print("Operaciones")
+    print("S. Suma")
+    print("R. Resta")
+    print("M. Multiplicacion")
+    print("D. Division")
+    print("A. Salir")
+
+if menu == "S":
+    num1 = ("Ingrese el numero 1:\n")
+    num2 = ("Ingrese el numero 2:\n")
+    resultado = num1 + num2
+    print(f"La suma es {resultado}")
+
+if menu == "R":
+    num1 = ("Ingrese el numero 1:\n")
+    num2 = ("Ingrese el numero 2:\n")
+    resultado = num1 - num2
+    print(f"La resta es {resultado}")
+    
+if menu == "M":
+    num1 = ("Ingrese el numero 1:\n")
+    num2 = ("Ingrese el numero 2:\n")
+    resultado = num1 * num2
+    print(f"La multiplicación es {resultado}")
+
+if menu == "D":
+    num1 = ("Ingrese el numero 1:\n")
+    num2 = ("Ingrese el numero 2:\n")
+    resultado = num1 / num2
+    print(f"La división es {resultado}")
+
+else:
+    break
+
+
+
